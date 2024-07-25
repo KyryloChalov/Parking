b6a959c7a5b3c39d8221fd50e1a9cc79d844797c
Yurii Protsenko <elsoul@gmail.com>
2024-07-25 22:19:37 +0300

    3 from 4 tasks vehicles

--- src/schemas/vehicles.py
+++ src/schemas/vehicles.py
@@ -4,6 +4,7 @@ from datetime import datetime, date
 from pydantic import BaseModel, EmailStr, Field
 from src.conf.constants import COMMENT_MAX_LENGTH, LICENSE_PLATE_MAX_LENGTH
 
+
 class BlacklistResposeSchema(BaseModel):
     id: int
     user_id: int
@@ -12,22 +13,51 @@ class BlacklistResposeSchema(BaseModel):
     updated_at: datetime | None
     reason: str = Field(max_length=COMMENT_MAX_LENGTH)
 
+
 class BLResposeSchema(BaseModel):
     license_plate: str
     created_at: datetime
     updated_at: datetime | None
     reason: str
+
     class Config:
         orm_mode = True
 
+
 class BlacklistedVehicleResponse(BLResposeSchema):
     username: str
-    owner_name : str | None
+    owner_name: str | None
     owner_email: str | None
+
 
 class BlacklistSchema(BaseModel):
     license_plate: str = Field(max_length=LICENSE_PLATE_MAX_LENGTH)
     reason: str = Field(max_length=COMMENT_MAX_LENGTH)
 
+
 class Reminder(BaseModel):
-    days : int
+    days: int
+
+
+class VehicleSchema(BaseModel):
+    license_plate: str = Field(max_length=LICENSE_PLATE_MAX_LENGTH)
+    owner_id: int
+    rate_id: int
+    created_at: datetime
+    updated_at: datetime | None
+
+
+class VehicleUpdateSchema(BaseModel):
+    license_plate: str = Field(max_length=LICENSE_PLATE_MAX_LENGTH)
+    owner_id: int
+    rate_id: int
+    updated_at: datetime | None
+
+
+class VehicleResponse(BaseModel):
+    id: int
+    license_plate: str = Field(max_length=LICENSE_PLATE_MAX_LENGTH)
+    owner_id: int
+    rate_id: int
+    created_at: datetime
+    updated_at: datetime | None
