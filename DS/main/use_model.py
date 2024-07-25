6108e5a05afcec3ffdb01f209bbcdb564b083d8a
Kyrylo Chalov <ks7977166@gmail.com>
2024-07-25 18:28:14 +0300

    repair 240725

--- DS/main/use_model.py
+++ DS/main/use_model.py
@@ -3,10 +3,6 @@ import os
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
-
-os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
-
 from keras.models import load_model
 from matplotlib import pyplot as plt
 
@@ -40,41 +36,9 @@ BOX_COLOR = (220, 220, 220)
 
 
 def display_image(img_, title="", recognized=True):
-import cv2
-import numpy as np
-import re
-
-from constants import IMAGES
-
-# from colors import YELLOW, BLUE, LIGHTBLUE, CYAN, GRAY, RESET
-
-YELLOW = "\033[33m"
-BLUE = "\033[34m"
-LIGHTBLUE = "\033[94m"
-RED = "\033[31m"
-CYAN = "\033[36m"
-GRAY = "\033[90m"
-RESET = "\033[0m"
-TAB = "\t"
-
-# from datetime import datetime
-
-PHOTO_FOLDER = "DS/images/"
-MODEL_CASCADE = "DS/models/haarcascade_ua_license_plate.xml"
-MODEL_KERAS = "DS/models/model_ua_license_plate.keras"
-TMP = "tmp/contour.jpg"
-BOX_COLOR = (220, 220, 220)
-
-
-# Функції:
-
-
-def display_image(img_, title=""):
     """
     функція виводу зображень з заголовком
     """
-    img_display = cv2.cvtColor(img_, cv2.COLOR_BGR2RGB)
-    # img = img_
     img_display = cv2.cvtColor(img_, cv2.COLOR_BGR2RGB)
 
     plt.figure(figsize=(10, 6))
@@ -140,7 +104,7 @@ def detect_plate(img_, text=""):
     return plate_img, plate_, plate_rect
 
 
-def find_contours(dimensions, img__, echo=True):
+def find_contours(dimensions, img_, echo=True):
     """
     Функція призначена для знаходження контурів символів на зображенні номерного знака.
 
@@ -155,7 +119,6 @@ def find_contours(dimensions, img__, echo=True):
 
     # Знайти всі контури на зображенні
     cntrs, _ = cv2.findContours(img_.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cntrs, _ = cv2.findContours(img_.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # Отримати потенційні розміри
     lower_width = dimensions[0]
@@ -167,18 +130,15 @@ def find_contours(dimensions, img__, echo=True):
     cntrs = sorted(cntrs, key=cv2.contourArea, reverse=True)[:15]
 
     ii = cv2.imread(tmp_file)
-    ii = cv2.imread(tmp_file)
 
     x_cntr_list = []
     img_res = []
-    # target_contours = []
     # target_contours = []
     for cntr in cntrs:
         # detects contour in binary image and returns the coordinates of rectangle enclosing it
         intX, intY, intWidth, intHeight = cv2.boundingRect(cntr)
 
         # checking the dimensions of the contour to filter out the characters by contour's size
-        # if all(intWidth > lower_width, intWidth < upper_width, intHeight > lower_height, intHeight < upper_height):
         # if all(intWidth > lower_width, intWidth < upper_width, intHeight > lower_height, intHeight < upper_height):
         if (
             intWidth > lower_width
@@ -189,13 +149,9 @@ def find_contours(dimensions, img__, echo=True):
             # stores the x coordinate of the character's contour,
             # to used later for indexing the contours
             x_cntr_list.append(intX)
-            # stores the x coordinate of the character's contour,
-            # to used later for indexing the contours
-            x_cntr_list.append(intX)
 
             char_copy = np.zeros((44, 24))
             # extracting each character using the enclosing rectangle's coordinates.
-            char = img_[intY : intY + intHeight, intX : intX + intWidth]
             char = img_[intY : intY + intHeight, intX : intX + intWidth]
             char = cv2.resize(char, (20, 40))
 
@@ -217,10 +173,7 @@ def find_contours(dimensions, img__, echo=True):
 
             # List that stores the character's binary image (unsorted)
             img_res.append(char_copy)
-            # List that stores the character's binary image (unsorted)
-            img_res.append(char_copy)
 
-    # Return characters on ascending order with respect to the x-coord (most-left character first)
     # Return characters on ascending order with respect to the x-coord (most-left character first)
     # if echo:
     #     plt.show()
@@ -237,7 +190,6 @@ def find_contours(dimensions, img__, echo=True):
 
 
 def segment_characters(image_, echo=True):
-def segment_characters(image_, echo=True):
     """
     Знаходить символи на зображенні номерного знака.
 
@@ -248,7 +200,6 @@ def segment_characters(image_, echo=True):
      - char_list: Список контурів символів, знайдених на зображенні.
     """
     # Попередньо оброблюємо зображення номерного знака
-    img_lp = cv2.resize(image_, (333, 75))  # Resize the image to a fixed size
     img_lp = cv2.resize(image_, (333, 75))  # Resize the image to a fixed size
     img_gray_lp = cv2.cvtColor(img_lp, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
 
@@ -264,10 +215,7 @@ def segment_characters(image_, echo=True):
 
     lp_width = img_binary_lp.shape[0]  # Get the width of the license plate
     lp_height = img_binary_lp.shape[1]  # Get the height of the license plate
-    lp_width = img_binary_lp.shape[0]  # Get the width of the license plate
-    lp_height = img_binary_lp.shape[1]  # Get the height of the license plate
 
-    # Робимо межі білими (3 пікселі)
     # Робимо межі білими (3 пікселі)
     img_binary_lp[0:3, :] = 255  # Minimum character height
     img_binary_lp[:, 0:3] = 255  # Maximum character height
@@ -276,7 +224,6 @@ def segment_characters(image_, echo=True):
 
     # Приблизні розміри контурів символів обрізаного номерного знака
     dimensions = [lp_width / 6, lp_width / 2, lp_height / 10, 2 * lp_height / 3]
-    dimensions = [lp_width / 6, lp_width / 2, lp_height / 10, 2 * lp_height / 3]
 
     if echo:
         plt.imshow(img_binary_lp, cmap="gray")  # Display the binary image
@@ -284,7 +231,6 @@ def segment_characters(image_, echo=True):
         plt.show()
 
     # Save the binary image to a file
-    cv2.imwrite(tmp_file, img_binary_lp)
     cv2.imwrite(tmp_file, img_binary_lp)
 
     # Get contours within cropped license plate
@@ -353,50 +299,7 @@ def validate_ukraine_plate(text):
 
 
 def prediction_number(char):
-def correction_ua_number(text: str) -> str:
-    # позиційна обробка ["1", "0", "7", '8'] <-> ["I", "O", "Z", 'B']
-    # if len(text) == 8:
-    text_list = list(text)
-
-    for i in [0, 1, 6, 7]:
-        text_list[i] = (
-            text_list[i]
-            .replace("1", "I")
-            .replace("i", "I")
-            .replace("|", "I")
-            .replace("0", "O")
-            .replace("7", "Z")
-            .replace("8", "B")
-            .replace("5", "B")
-        )
-
-    for i in [2, 3, 4, 5]:
-        text_list[i] = (
-            text_list[i]
-            .replace("I", "1")
-            .replace("|", "1")
-            .replace("O", "0")
-            .replace("Z", "7")
-            .replace("B", "8")
-            .replace("G", "6")
-            .replace("J", "3")
-        )
-
-    text = "".join(text_list)
-    return text
-
-
-def validate_ukraine_plate(text):
     """
-    Validate Ukrainian plate format
-    """
-    pattern = r"^[A-Z]{2}\d{4}[A-Z]{2}$"
-    return bool(re.match(pattern, text))
-
-
-def prediction_number(char):
-    """
-    Функція для розпізнавання символів на номерному знаку.
     Функція для розпізнавання символів на номерному знаку.
     Параметри:
     char (list): Список зображень символів номерного знаку.
@@ -413,10 +316,6 @@ def prediction_number(char):
         img_ = cv2.resize(ch, (28, 28), interpolation=cv2.INTER_AREA)
         img = fix_dimension(img_)
         img = img.reshape(1, 28, 28, 3)  # підготовка зображення для моделі
-
-        # отримуємо ймовірності для кожного класу
-        y_proba = model.predict(img, verbose=0)[0]
-
 
         # отримуємо ймовірності для кожного класу
         y_proba = model.predict(img, verbose=0)[0]
@@ -455,30 +354,7 @@ def make_final_image(
     cv2.rectangle(img_, box_coords[0], box_coords[1], BOX_COLOR, cv2.FILLED)
 
     color = (0, 155, 0) if recognized else (0, 0, 200)
-    # Calculate the position for the text near the bottom of the image
-    img_height, _ = img_.shape[:2]
-    text_position = (50, img_height - 50)
-
-    # Calculate the size of the text box
-    (text_width, text_height), baseline = cv2.getTextSize(
-        recognized_text, font, font_scale, font_thickness
-    )
-
-    # Calculate the coordinates for the white rectangle
-    box_coords = (
-        (text_position[0] - 10, text_position[1] + baseline - text_height - 30),
-        (text_position[0] + text_width + 10, text_position[1] + baseline - 10),
-    )
-
-    # Draw the white rectangle
-    cv2.rectangle(img_, box_coords[0], box_coords[1], BOX_COLOR, cv2.FILLED)
-
-    color = (0, 0, 200) if (color_ == RED) else (0, 155, 0)
     # Add recognized text to the image
-    img_output = cv2.putText(
-        # Copy the image to avoid modifying the original one ???
-        # img_.copy(),
-        img_,
     img_output = cv2.putText(
         # Copy the image to avoid modifying the original one ???
         # img_.copy(),
@@ -487,7 +363,6 @@ def make_final_image(
         text_position,
         font,
         font_scale,
-        color,
         color,
         font_thickness,
         cv2.LINE_AA,
@@ -538,56 +413,9 @@ def processing(photo, echo=True, log_on=False):
         )
     return plate_number_, recognized
 
-    return img_output
-
-
-def processing(photo, echo=True, log_on=False) -> str:
-    """
-    основна функція розпізнавання номеру
-    """
-    car_photo = os.path.join(current_dir, PHOTO_FOLDER, photo)
-    car_photo_imread = cv2.imread(car_photo)
-    if log_on:
-        print(f"{GRAY}{photo = }{RESET}")
-
-    try:
-        output_img, plate = detect_plate(car_photo_imread)
-        license_plate_symbols = segment_characters(plate, echo=False)
-        plate_number_ = prediction_number(license_plate_symbols)
-    except UnboundLocalError:
-        output_img = car_photo_imread
-        plate_number_ = "NOT RECOGNIZED"
-
-    if len(plate_number_) == 8:
-        plate_number_ = correction_ua_number(plate_number_)
-
-    number_color = ""
-    # if re.match(r"^[A-Z0-9]+$", plate_number_):
-    if not validate_ukraine_plate(plate_number_):
-        recognized = False
-        number_color = RED
-    else:
-        recognized = True
-        number_color = LIGHTBLUE
-
-    if log_on:
-        print(number_color, TAB, plate_number_, RESET)
-
-    if echo:
-        img_result = make_final_image(
-        output_img, recognized_text=plate_number_, color_=number_color
-        )
-        display_image(
-            img_result,
-            title="Номерний знак" + (" НЕ" if not recognized else "") + " розпізнано",
-        )
-    return plate_number_, recognized
-
 
 # loading the data required for detecting the license plates using cascade classifier.
 current_dir = os.getcwd()
-
-plate_cascade_path = os.path.join(current_dir, MODEL_CASCADE)
 
 plate_cascade_path = os.path.join(current_dir, MODEL_CASCADE)
 plate_cascade = cv2.CascadeClassifier(plate_cascade_path)
