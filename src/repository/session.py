8fcb5a3d02e2abbc136288bd12529cd6d5a099c2
IvanBosovch <138058545+IvanBosovch@users.noreply.github.com>
2024-07-27 11:07:49 +0300

    Change routes sessions
    1. Add sessions in table session

--- src/repository/session.py
+++ src/repository/session.py
@@ -1,18 +1,30 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from sqlalchemy.orm import Session as DbSession
-from src.models.models import Vehicle as DbSessionModel
+from src.models.models import Parking_session as DbSessionModel
 from src.schemas.session import SessionCreate, SessionClose
 import uuid
 from datetime import datetime
 from fastapi import HTTPException
+from src.models.models import Vehicle
 
 async def create_session(session_data: SessionCreate, db: AsyncSession) -> DbSessionModel:
-    session = DbSessionModel(   
-        license_plate=session_data.number,
-        rate_id = 1,
+    vehicle_query = select(Vehicle).where(Vehicle.license_plate == session_data.number)
+    vehicle_result = await db.execute(vehicle_query)
+    vehicle = vehicle_result.scalars().first()
+    
+    if not vehicle:
+        raise HTTPException(status_code=404, detail="Vehicle not found")
+
+    total_cost = 0  
+    empty_place = 0  
+
+    session = DbSessionModel(
+        vehicle_id=vehicle.id,
+        total_cost=total_cost,
+        empty_place=empty_place,
         created_at=datetime.now()
     )
+    
     db.add(session)
     await db.commit()
     await db.refresh(session)
@@ -20,7 +32,7 @@ async def create_session(session_data: SessionCreate, db: AsyncSession) -> DbSes
 
 
 async def close_session(session_data: SessionClose, db: AsyncSession) -> DbSessionModel:
-    query = select(DbSessionModel).where(DbSessionModel.license_plate == session_data.number, DbSessionModel.updated_at.is_(None))
+    query = select(DbSessionModel).where(Vehicle.license_plate == session_data.number, DbSessionModel.updated_at.is_(None))
     result = await db.execute(query)
     session = result.scalars().first()
     
@@ -31,9 +43,3 @@ async def close_session(session_data: SessionClose, db: AsyncSession) -> DbSessi
     await db.commit()
     await db.refresh(session)
     return session
-
-
-def verify_image(image: bytes) -> bool:
-    # Тут буде логіка перевірки зображення
-    # Повертає True, якщо зображення пройшло перевірку, інакше False
-    return True
--- src/routes/session.py
+++ src/routes/session.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, UploadFile, File, Depends, APIRouter
+from fastapi import UploadFile, File, Depends, APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.repository import session as session_repo
 from src.schemas.session import SessionCreate, SessionClose
@@ -13,12 +13,10 @@ access_to_route_all = RoleAccess([Role.admin])
 @router.post("/in", dependencies=[Depends(access_to_route_all)])
 async def in_session(image: UploadFile = File(...), db: AsyncSession = Depends(get_db)):
     image_bytes = await image.read()
-    if not session_repo.verify_image(image_bytes):
-        raise HTTPException(status_code=400, detail="Image verification failed")
     
     # Розпізнавання номера
     #number = processing(image_bytes)   Функція розпізнавання, у мене не працює TensorFlow, не можу перевірити
-    number = 'FH1234HN'
+    number = 'Car'
 
     session = await session_repo.create_session(SessionCreate(number=number), db)
     return {"session_id": session.id}
@@ -33,12 +31,10 @@ async def manual_in_session(number: str, db: AsyncSession = Depends(get_db)):
 @router.post("/out", dependencies=[Depends(access_to_route_all)])
 async def out_session(image: UploadFile = File(...), db: AsyncSession = Depends(get_db)):
     image_bytes = await image.read()
-    if not session_repo.verify_image(image_bytes):
-        raise HTTPException(status_code=400, detail="Image verification failed")
     
     # Розпізнавання номера
     # number = processing(image_bytes) , Функція розпізнавання, у мене не працює TensorFlow, не можу перевірити
-    number = 'FH1234HN'
+    number = 'Car'
     
     session = await session_repo.close_session(SessionClose(number=number), db)
     return {"session_id": session.id}
