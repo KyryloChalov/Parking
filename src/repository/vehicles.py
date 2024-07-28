615d5bc4214b1c497e732afaf4e16a70f1d00c9b
Jurij Procenko <97167569+JurijProcenko@users.noreply.github.com>
2024-07-28 12:42:50 +0300

    added car by username (#23)

--- src/repository/vehicles.py
+++ src/repository/vehicles.py
@@ -30,6 +30,18 @@ async def get_vehicle_by_plate(license_plate: str, db: AsyncSession = Depends(ge
         return None
 
 
+async def find_user_id_by_username(username: str, db: AsyncSession) -> int:
+
+    query = select(User.id).where(User.username == username)
+    result = await db.execute(query)
+    user_id = result.scalar_one_or_none()
+    if not user_id:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=messages.USER_NOT_FOUND
+        )
+    return user_id
+
+
 async def get_vehicle_in_black_list(license_plate: str, db: AsyncSession):
 
     vehicle = await get_vehicle_by_plate(license_plate, db)
@@ -241,12 +253,9 @@ async def get_user_by_license_plate(license_plate: str, db: AsyncSession):
     return user_vehicle
 
 
-async def add_to_DB(
-    body: VehicleSchema, user: User, db: AsyncSession = Depends(get_db)
-):
-
+async def add_to_DB(body: VehicleSchema, user_id: int, db: AsyncSession):
     license_plate = Vehicle(
-        license_plate=body.license_plate, owner_id=body.owner_id, rate_id=body.rate_id
+        license_plate=body.license_plate, owner_id=user_id, rate_id=body.rate_id
     )
     db.add(license_plate)
     await db.commit()
@@ -266,7 +275,7 @@ async def get_all_vehicles_reminder(db: AsyncSession):
     result = await db.execute(stmt)
     setting = result.scalar_one_or_none()
     days_reminder = datetime.now().date() + timedelta(days=setting.num_days_reminder)
-    
+
     stmt = select(Vehicle).where(Vehicle.ended_at <= days_reminder)
     result = await db.execute(stmt)
     vehicles = result.scalars().all()
@@ -275,7 +284,7 @@ async def get_all_vehicles_reminder(db: AsyncSession):
     else:
         return None
 
-    
+
 async def get_vehicles_not_in_black_list(db: AsyncSession):
     """
     The get_all_black_list function returns a list of all vehicles in the black_list.
@@ -286,11 +295,11 @@ async def get_vehicles_not_in_black_list(db: AsyncSession):
     :return: A list of vehicles in black list
     """
     blacklist_alias = aliased(Blacklist)
-    # outerjoin: Використовується для об'єднання таблиць, де в результаті включаються всі записи 
-    # з лівої таблиці (vehicles) та відповідні записи з правої таблиці (blacklists). Якщо в правій 
+    # outerjoin: Використовується для об'єднання таблиць, де в результаті включаються всі записи
+    # з лівої таблиці (vehicles) та відповідні записи з правої таблиці (blacklists). Якщо в правій
     # таблиці немає відповідного запису, то всі колонки з правої таблиці будуть заповнені NULL.
-    # where(blacklist_alias.vehicle_id == None): Ця умова вибирає тільки ті записи, 
-    # де немає відповідного запису в таблиці blacklists, тобто автомобіль не знаходиться 
+    # where(blacklist_alias.vehicle_id == None): Ця умова вибирає тільки ті записи,
+    # де немає відповідного запису в таблиці blacklists, тобто автомобіль не знаходиться
     # в чорному списку.
     stmt = (
         select(Vehicle)
@@ -300,8 +309,9 @@ async def get_vehicles_not_in_black_list(db: AsyncSession):
     )
     result = await db.execute(stmt)
     vehicles_without_black_list = result.scalars().all()
-    
+
     return vehicles_without_black_list
+
 
 async def get_vehicles_with_abonement(db: AsyncSession):
     stmt = select(func.count()).select_from(Vehicle).where(Vehicle.ended_at != None)
@@ -309,12 +319,14 @@ async def get_vehicles_with_abonement(db: AsyncSession):
     num_vehicles_abonement = result.scalar()
     return num_vehicles_abonement
 
+
 async def free_parking_space(db: AsyncSession):
-    stmt = (select(func.count())
-    .select_from(Parking_session)
-    .where(Parking_session.updated_at == None)
-    .outerjoin(Vehicle, Vehicle.id == Parking_session.vehicle_id)
-    .where(Vehicle.ended_at == None)
+    stmt = (
+        select(func.count())
+        .select_from(Parking_session)
+        .where(Parking_session.updated_at == None)
+        .outerjoin(Vehicle, Vehicle.id == Parking_session.vehicle_id)
+        .where(Vehicle.ended_at == None)
     )
     result: int = await db.execute(stmt)
     num_vehicles_not_abonement = result.scalar()
@@ -325,7 +337,6 @@ async def free_parking_space(db: AsyncSession):
     num_vehicles_abonement = await get_vehicles_with_abonement(db)
     free_space = setting.capacity - num_vehicles_not_abonement - num_vehicles_abonement
     return free_space
-
 
 
 async def update_vehicle(
