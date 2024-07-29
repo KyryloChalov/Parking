e4f9439dab90176409257ebb3cd13eca254675bb
Kyrylo Chalov <ks7977166@gmail.com>
2024-07-29 19:22:25 +0300

    sessions - fix text

--- src/repository/session.py
+++ src/repository/session.py
@@ -5,57 +5,39 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from fastapi import HTTPException, status
 
-from src.repository.vehicles import get_vehicle_in_black_list, get_vehicle_by_plate, add_vehicle_to_db_auto
-# <<<<<<< oleksandr
+from src.repository.vehicles import (
+    get_vehicle_in_black_list,
+    get_vehicle_by_plate,
+    add_vehicle_to_db_auto,
+)
 from src.models.models import Parking_session
 from src.conf import messages
-# from src.schemas.session import SessionCreate, SessionClose
-
-
-# =======
-# from src.models.models import Parking_session
 from src.models.models import Vehicle, Parking_session, Blacklist, Rate
-# from src.conf import messages
 from src.schemas.session import SessionCreate, SessionClose
-# <<<<<<< oleksandr
 
 
-# >>>>>>> dev
 async def create_session(license_plate, db: AsyncSession):
     vehicle = await get_vehicle_in_black_list(license_plate, db)
     if vehicle:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Entrance closed. Auto in black list')
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Entrance closed. Auto in black list",
+        )
     vehicle = await get_vehicle_by_plate(license_plate, db)
     if not vehicle:
         vehicle = await add_vehicle_to_db_auto(license_plate, db)
-# <<<<<<< oleksandr
-    
+
     stmt = select(Parking_session).where(Parking_session.vehicle_id == vehicle.id)
     result = await db.execute(stmt)
     vehicle_in_parking = result.scalar_one_or_none()
     if vehicle_in_parking:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Entrance closed. Auto in the parking yet!')
-    
-    stmt = Parking_session(   
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Entrance closed. Auto in the parking yet!",
+        )
+
+    stmt = Parking_session(
         vehicle_id=vehicle.id,
-# =======
-#     stmt = Parking_session(   
-#         vehicle_id=vehicle.id,
-# =======
-# import uuid
-# from datetime import datetime
-# from fastapi import HTTPException
-# from sqlalchemy.ext.asyncio import AsyncSession
-# from src.models.models import Vehicle, Parking_session, Blacklist, Rate
-
-
-# async def create_session(
-#     session_data: SessionCreate, db: AsyncSession
-# ) -> DbSessionModel:
-#     session = DbSessionModel(
-#         license_plate=session_data.number, rate_id=1, created_at=datetime.now()
-# >>>>>>> dev
-# >>>>>>> dev
     )
     db.add(stmt)
     await db.commit()
@@ -66,32 +48,29 @@ async def create_session(license_plate, db: AsyncSession):
 async def close_session(license_plate: str, db: AsyncSession):
     vehicle = await get_vehicle_in_black_list(license_plate, db)
     if vehicle:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Exit closed. Auto in black list')
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Exit closed. Auto in black list",
+        )
     vehicle = await get_vehicle_by_plate(license_plate, db)
     if not vehicle:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=messages.VEHICLE_NOT_FOUND)
-    stmt = select(Parking_session).where(and_(Parking_session.vehicle_id == vehicle.id, Parking_session.updated_at == None))
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=messages.VEHICLE_NOT_FOUND
+        )
+    stmt = select(Parking_session).where(
+        and_(
+            Parking_session.vehicle_id == vehicle.id, Parking_session.updated_at == None
+        )
+    )
     result = await db.execute(stmt)
     session = result.scalar_one_or_none()
-    
+
     if not session:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Session not found or already closed")
-    
-# =======
-# async def close_session(session_data: SessionClose, db: AsyncSession) -> DbSessionModel:
-#     query = select(DbSessionModel).where(
-#         DbSessionModel.license_plate == session_data.number,
-#         DbSessionModel.updated_at.is_(None),
-#     )
-#     result = await db.execute(query)
-#     session = result.scalars().first()
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Session not found or already closed",
+        )
 
-#     if not session:
-#         raise HTTPException(
-#             status_code=404, detail="Session not found or already closed"
-#         )
-
-# >>>>>>> dev
     session.updated_at = datetime.now()
     await db.commit()
     await db.refresh(session)
@@ -137,6 +116,7 @@ async def create_parking_session(vehicle_id: int, db: AsyncSession):
     await db.commit()
     await db.refresh(new_session)
     return new_session
+
 
 # For manual_out
 async def close_parking_session(session_id: int, db: AsyncSession):
--- src/routes/session.py
+++ src/routes/session.py
@@ -1,16 +1,7 @@
 import re
 from fastapi import FastAPI, HTTPException, UploadFile, File, Depends, APIRouter, status
 from sqlalchemy.ext.asyncio import AsyncSession
-# <<<<<<< oleksandr
-# from src.repository import session
-# from src.schemas.session import SessionCreate, SessionClose
-# from src.services.roles import RoleAccess
-# from src.models.models import Role
-# from src.database.db import get_db
-# from src.services.use_model import processing
 
-# =======
-# <<<<<<< oleksandr
 from src.repository import session
 from src.schemas.session import SessionCreate, SessionClose
 from src.services.roles import RoleAccess
@@ -21,10 +12,6 @@ from src.services.use_model import processing
 from src.repository import vehicles as repositories_vehicles
 from src.services.auth import auth_service
 
-
-# # from DS.main.use_model import processing
-# >>>>>>> dev
-# >>>>>>> dev
 router = APIRouter(prefix="/session", tags=["session"])
 access_to_route_all = RoleAccess([Role.admin, Role.operator])
 
@@ -34,36 +21,15 @@ LICENSE_PLATE_REGEX = re.compile(r"^[A-Za-zА-Яа-я]{2}\d{4}[A-Za-zА-Яа-я]
 @router.post("/in", dependencies=[Depends(access_to_route_all)])
 async def in_session(image: UploadFile = File(...), db: AsyncSession = Depends(get_db)):
     image_bytes = await image.read()
-# <<<<<<< oleksandr
 
     number, recognize = processing(image_bytes, echo=False, log_on=False)
     if not recognize:
-        raise HTTPException(status_code=406, detail=f"Image verification failed - {number}")
+        raise HTTPException(
+            status_code=406, detail=f"Image verification failed - {number}"
+        )
 
     result = await session.create_session(number, db)
     return {"session_id": result.id, "plate_number": number}
-
-# @router.post("/manual_in", dependencies=[Depends(access_to_route_all)])
-# async def manual_in_session(number: str, db: AsyncSession = Depends(get_db)):
-#     session = await session.create_session(SessionCreate(number=number), db)
-#     return {"session_id": session.id}
-# =======
-# <<<<<<< oleksandr
-# =======
-#     if not session_repo.verify_image(image_bytes):
-#         raise HTTPException(status_code=400, detail="Image verification failed")
-
-#     # Розпізнавання номера
-#     # number = processing(image_bytes)   Функція розпізнавання, у мене не працює TensorFlow, не можу перевірити
-#     number = "FH1234HN"
-# >>>>>>> dev
-
-#     number, recognize = processing(image_bytes, echo=False, log_on=False)
-#     if not recognize:
-#         raise HTTPException(status_code=406, detail="Image verification failed")
-
-#     result = await session.create_session(number, db)
-#     return {"session_id": result.id, "plate_number": number}
 
 
 #  Manual_in
@@ -109,8 +75,6 @@ async def manual_in(
     new_session = await session.create_parking_session(vehicle.id, db)
 
     return {"session_id": new_session.id}
-# >>>>>>> dev
-# >>>>>>> dev
 
 
 @router.post("/out", dependencies=[Depends(access_to_route_all)])
@@ -118,22 +82,14 @@ async def out_session(
     image: UploadFile = File(...), db: AsyncSession = Depends(get_db)
 ):
     image_bytes = await image.read()
-# <<<<<<< oleksandr
     number, recognize = processing(image_bytes, echo=False, log_on=False)
     if not recognize:
-        raise HTTPException(status_code=406, detail=f"Image verification failed - {number}")
-    
+        raise HTTPException(
+            status_code=406, detail=f"Image verification failed - {number}"
+        )
+
     result = await session.close_session(number, db)
     return {"session_id": result.id, "plate_number": number}
-# =======
-# # <<<<<<< oleksandr
-#     number, recognize = processing(image_bytes, echo=False, log_on=False)
-#     if not recognize:
-#         raise HTTPException(status_code=406, detail="Image verification failed")
-    
-#     result = await session.close_session(number, db)
-#     return {"session_id": result.id, "plate_number": number}
-
 
 
 @router.post("/manual_out")
@@ -168,15 +124,7 @@ async def manual_out(
             status_code=status.HTTP_409_CONFLICT,
             detail="No open parking session found for this vehicle.",
         )
-# >>>>>>> dev
 
     closed_session = await session.close_parking_session(open_session.id, db)
 
-# <<<<<<< oleksandr
-# @router.post("/manual_out", dependencies=[Depends(access_to_route_all)])
-# async def manual_out_session(number: str, db: AsyncSession = Depends(get_db)):
-#     result = await session.close_session(SessionClose(number=number), db)
-#     return {"session_id": result.id}
-# =======
     return {"session_id": closed_session.id}
-# >>>>>>> dev
